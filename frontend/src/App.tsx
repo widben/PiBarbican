e8a9dbb864bdee01a892b1b7b90c9bbdda3cfcb9
Benjamin Widmann <benjamin.widmann@me.com>
2020-01-08 15:30:29 +0100

    vs code workspace, blueprint3 ui framework

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,12 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import {H1} from "@blueprintjs/core";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-};
-
-export default App;
+export default class App extends React.PureComponent {
+  public render = () => {
+    return (
+      <div className="App">
+        <H1>PiBarbican</H1>
+      </div>
+    );
+  }
+}
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import "normalize.css/normalize.css";
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
